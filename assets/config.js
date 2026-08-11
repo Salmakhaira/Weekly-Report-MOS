70a65067cf2245fe68cfe3805184145775a99cf5
Salmakhaira <114233503+Salmakhaira@users.noreply.github.com>
2026-08-11 10:18:07 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -10,8 +10,8 @@
    JANGAN pernah menaruh service_role key di sini.
    ===================================================================== */
 
-export const SUPABASE_URL = 'https://GANTI-DENGAN-PROJECT-ANDA.supabase.co';
-export const SUPABASE_ANON_KEY = 'GANTI-DENGAN-ANON-PUBLIC-KEY';
+export const SUPABASE_URL = 'https://hxdcyylbtwzyegzwppil.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4ZGN5eWxidHd6eWVnendwcGlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0MDc3MDAsImV4cCI6MjEwMTk4MzcwMH0.1kpt3-abdYZvAhpf-PP8Up9ytW3_RXsReMgXuG56Y38';
 
 /* Judul yang tampil di header. Ubah sesuai nama perusahaan. */
 export const APP_NAME = 'Weekly Report MOS';
