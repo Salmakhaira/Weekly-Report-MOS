e363668950ceeda57c77e5ba23bb3622655c9021
Salmakhaira <114233503+Salmakhaira@users.noreply.github.com>
2026-08-26 11:21:33 +0700

    Delete assets directory

--- assets/config.js
+++
@@ -1,6 +0,0 @@
-export const SUPABASE_URL = 'https://hxdcyylbtwzyegzwppil.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4ZGN5eWxidHd6eWVnendwcGlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0MDc3MDAsImV4cCI6MjEwMTk4MzcwMH0.1kpt3-abdYZvAhpf-PP8Up9ytW3_RXsReMgXuG56Y38';
-
-/* Judul yang tampil di header */
-export const APP_NAME = 'Weekly Report MOS';
-export const APP_SUB  = 'Form';
